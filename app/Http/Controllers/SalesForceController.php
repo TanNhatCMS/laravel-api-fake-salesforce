033473401e85223add2776b598454849806302ec
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-09-16 02:30:22 +0700

    add bookings api

--- app/Http/Controllers/SalesForceController.php
+++ app/Http/Controllers/SalesForceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Services\JWTService;
+use Illuminate\Support\Facades\Validator;
 
 class SalesForceController extends Controller
 {
@@ -43,5 +44,46 @@ class SalesForceController extends Controller
             'payload' => $payload
         ];
         return response()->json($response, 200);
+    }
+
+    /**
+     * Operation bookings
+     *
+     * Tạo đặt chỗ mới.
+     *
+     *
+     * @return JsonResponse response
+     */
+    public function bookings(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'FacilityNumber' => 'required|string|max:80',
+            'PersonLastName' => 'required|string|max:255',
+            'PersonFirstName' => 'required|string|max:255',
+            'ChildBirthday1' => 'required|date',
+            'ChildBirthday2' => 'nullable|date',
+            'PersonContactPhone' => 'required|string|max:15',
+            'PersonContactMail' => 'nullable|email|max:255',
+            'DesiredReservationDateTime' => 'required|date_format:Y-m-d\TH:i:s',
+            'DesiredAdmissionDate' => 'required|date',
+            'PersonAddress' => 'required|string|max:255',
+            'PersonNumber' => 'required|integer|min:1',
+            'ChildNumber' => 'required|integer|min:1',
+            'MessageToChildcareFacility' => 'nullable|string|max:2000',
+        ]);
+        // If validation fails, return an error response
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => 'Invalid data',
+                'messages' => $validator->errors()
+            ], 400);
+        }
+        $randomNumber = 'BK-' . str_pad(rand(0, 99999999), 8, '0', STR_PAD_LEFT);
+        return response()->json([
+            'ReservationRequiryNumber' => $randomNumber,
+            'ReservationRequiryStatus' => '仮予約',
+            'payload' => $data
+        ], 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ use App\Http\Controllers\SalesForceController;
 Route::get('/', function () {
     return view('welcome');
 });
-/**
- * post servicesOauth2TokenPOST
- * Summary: Lấy JWT access_token và instance_url test.salesforce.com
- * Notes:
- * Output-Formats: [application/json]
- */
-Route::post('/services/oauth2/token', [SalesForceController::class, 'getToken']);
+Route::prefix('services')->group(function () {
+    /**
+     * post servicesOauth2TokenPOST
+     * Summary: Lấy JWT access_token và instance_url test.salesforce.com
+     * Notes:
+     * Output-Formats: [application/json]
+     */
+    Route::post('/oauth2/token', [SalesForceController::class, 'getToken']);
+    Route::post('/apexrest/hkt/v1.0/bookings', [SalesForceController::class, 'bookings']);
+});
+
